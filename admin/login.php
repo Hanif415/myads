d3d2f18f1de3154411b3f44fd963a037c9668276
hanif415 <hanifamrullah42@gmail.com>
2023-09-13 12:40:56 +0700

    admin fixed

--- admin/login.php
+++ admin/login.php
@@ -3,10 +3,10 @@
 session_start();
 
 // Check if the user is already logged in, if yes then redirect him to welcome page
-if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-    header("location: index.php");
-    exit;
-}
+// if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+//     header("location: index.php");
+//     exit;
+// }
 
 // Include config file
 require_once "backend/config.php";
@@ -150,7 +150,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                         </div>
 
                                         <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                            <button type="submit" class="btn btn-primary btn-lg">Register</button>
+                                            <button type="submit" class="btn btn-primary btn-lg">Sign In</button>
                                         </div>
                                         <p>Don't have an account? <a href="register.php">Register here</a>.</p>
                                     </form>
@@ -171,7 +171,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
 </body>
--- admin/register.php
+++ admin/register.php
@@ -119,15 +119,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+    <style>
+    .register-form {
+        padding-top: 50px;
+        padding-bottom: 50px;
+    }
+    </style>
 
     <title>Register</title>
 </head>
 
 <body>
-    <section class="vh-100" style="background-color: #eee;">
-        <div class="container h-100">
+    <section class="h-100" style="background-color: #eee;">
+        <div class="container register-form">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-lg-12 col-xl-11">
                     <div class="card text-black" style="border-radius: 25px;">
@@ -137,13 +144,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                                     <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
 
-                                    <form class="mx-1 mx-md-4" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                                    <form class="mx-1 mx-md-4"
+                                        action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
 
                                         <div class="d-flex flex-row align-items-center mb-4">
                                             <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                                             <div class="form-outline flex-fill mb-0">
                                                 <label class="form-label" for="name">Your Name</label>
-                                                <input type="text" id="name" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>" name="name" />
+                                                <input type="text" id="name"
+                                                    class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>"
+                                                    value="<?php echo $name; ?>" name="name" />
                                                 <span class="invalid-feedback"><?php echo $name_err; ?></span>
                                             </div>
                                         </div>
@@ -152,7 +162,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                             <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                                             <div class="form-outline flex-fill mb-0">
                                                 <label class="form-label" for="username">Username</label>
-                                                <input type="text" id="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>" name="username" />
+                                                <input type="text" id="username"
+                                                    class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>"
+                                                    value="<?php echo $username; ?>" name="username" />
                                                 <span class="invalid-feedback"><?php echo $username_err; ?></span>
                                             </div>
                                         </div>
@@ -161,7 +173,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                             <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                                             <div class="form-outline flex-fill mb-0">
                                                 <label class="form-label" for="password">Password</label>
-                                                <input type="password" id="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>" name="password" />
+                                                <input type="password" id="password"
+                                                    class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>"
+                                                    value="<?php echo $password; ?>" name="password" />
                                                 <span class="invalid-feedback"><?php echo $password_err; ?></span>
                                             </div>
                                         </div>
@@ -171,7 +185,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                             <div class="form-outline flex-fill mb-0">
                                                 <label class="form-label" for="confirm_password">Repeat your
                                                     password</label>
-                                                <input type="password" id="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" name="confirm_password" />
+                                                <input type="password" id="confirm_password"
+                                                    class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>"
+                                                    name="confirm_password" />
                                                 <span class="invalid-feedback">
                                                     <?php echo $confirm_password_err; ?></span>
                                             </div>
@@ -186,7 +202,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                 </div>
                                 <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
 
-                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp" class="img-fluid" alt="Sample image">
+                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                                        class="img-fluid" alt="Sample image">
 
                                 </div>
                             </div>
@@ -199,8 +216,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
 </body>
 
