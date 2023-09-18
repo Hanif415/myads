e833085ab52df66a6cc99d25e254f55e96c59323
hanif415 <hanifamrullah42@gmail.com>
2023-09-18 17:29:42 +0700

    pagination done

--- backend/getAllBlog.php
+++ backend/getAllBlog.php
@@ -1,5 +1,20 @@
 <?php
-$queryAllBlog = "SELECT * FROM `blogs` ORDER BY published_at DESC";
+
+$batas = 3;
+$halaman = isset($_GET['halaman']) ? (int)$_GET['halaman'] : 1;
+$halaman_awal = ($halaman > 1) ? ($halaman * $batas) - $batas : 0;
+
+$previous = $halaman - 1;
+$next = $halaman + 1;
+
+$data = mysqli_query($link, "select * from blogs");
+$jumlah_data = mysqli_num_rows($data);
+$total_halaman = ceil($jumlah_data / $batas);
+
+// $data_pegawai = mysqli_query($koneksi, "select * from pegawai limit $halaman_awal, $batas");
+// $nomor = $halaman_awal + 1;
+
+$queryAllBlog = "SELECT * FROM blogs ORDER BY published_at DESC limit $halaman_awal, $batas";
 $blogs = "";
 // FETCHING DATA FROM DATABASE
 $resultAllBlog = mysqli_query($link, $queryAllBlog);
--- layouts/blogs/index.php
+++ layouts/blogs/index.php
@@ -136,6 +136,27 @@ include('../../backend/getNewBlog.php');
                 }
                 ?>
             </div>
+            <nav>
+                <ul class="pagination justify-content-center">
+                    <li class="page-item">
+                        <a class="page-link" <?php if ($halaman > 1) {
+                                                    echo "href='?halaman=$previous'";
+                                                } ?>>Previous</a>
+                    </li>
+                    <?php
+                    for ($x = 1; $x <= $total_halaman; $x++) {
+                    ?>
+                        <li class="page-item"><a class="page-link" href="?halaman=<?php echo $x ?>"><?php echo $x; ?></a></li>
+                    <?php
+                    }
+                    ?>
+                    <li class="page-item">
+                        <a class="page-link" <?php if ($halaman < $total_halaman) {
+                                                    echo "href='?halaman=$next'";
+                                                } ?>>Next</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </div>
     <footer>
