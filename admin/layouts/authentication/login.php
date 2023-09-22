7da1d8e270e414af4b8f79a8f2ee3e5ce0aaa899
hanif415 <hanifamrullah42@gmail.com>
2023-09-22 12:28:18 +0700

    fix

--- admin/layouts/authentication/login.php
+++ admin/layouts/authentication/login.php
@@ -25,7 +25,7 @@ include('../../backend/authentication/login.php');
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 
-    <title>Register</title>
+    <title>Sign In</title>
 </head>
 
 <body>
@@ -36,7 +36,7 @@ include('../../backend/authentication/login.php');
                     <div class="card text-black" style="border-radius: 25px;">
                         <div class="card-body p-md-5">
                             <div class="row justify-content-center">
-                                <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                                <div class="col-md-10 col-lg-6 order-2 order-lg-1">
 
                                     <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign In</p>
 
@@ -75,16 +75,14 @@ include('../../backend/authentication/login.php');
                                         </div>
 
                                         <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                            <button type="submit" class="btn btn-primary btn-lg">Sign In</button>
+                                            <button type="submit" class="btn btn-success btn-lg">Sign In</button>
                                         </div>
                                         <p>Don't have an account? <a href="register.php">Register here</a>.</p>
                                     </form>
 
                                 </div>
-                                <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-
-                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp" class="img-fluid" alt="Sample image">
-
+                                <div class="col-md-10 col-lg-4 d-flex align-items-center justify-content-center order-1 order-lg-2">
+                                    <img src="../../../assets/images/myads.jpg" class="img-fluid" alt="Sample image">
                                 </div>
                             </div>
                         </div>
