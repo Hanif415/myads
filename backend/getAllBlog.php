0991e54085ea51e808a88448dba3c314f1c37430
hanif415 <hanifamrullah42@gmail.com>
2023-09-22 14:44:46 +0700

    fix

--- backend/getAllBlog.php
+++ backend/getAllBlog.php
@@ -2,7 +2,7 @@
 
 $search = "";
 
-if (!empty($_GET['search'])) {
+if (isset($_GET["search"]) && !empty($_GET['search'])) {
     $search = $_GET['search'];
     $data = mysqli_query($link, "SELECT * FROM blogs WHERE title LIKE '%$search%' OR body LIKE '%$search%'");
 
@@ -21,42 +21,40 @@ if (!empty($_GET['search'])) {
     $blogs = "";
     // FETCHING DATA FROM DATABASE
     $resultAllBlog = mysqli_query($link, $queryAllBlog);
+} else if (isset($_GET["category"])) {
+    $slug = $_GET["category"];
+    $categories = mysqli_query($link, "SELECT * FROM categories WHERE slug = '$slug'");
+    $category = mysqli_fetch_assoc($categories);
+    $category_id = $category["id"];
+    $data = mysqli_query($link, "SELECT * FROM blogs WHERE category_id = $category_id");
+    $total_data = mysqli_num_rows($data);
+    $limit = 12;
+    $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+    $first_page = ($page > 1) ? ($page * $limit) - $limit : 0;
+    $total_page = "";
+
+    $previous = $page - 1;
+    $next = $page + 1;
+    $total_page = ceil($total_data / $limit);
+
+    $queryAllBlog = "SELECT * FROM blogs WHERE category_id = $category_id ORDER BY published_at DESC limit $first_page, $limit";
+    $blogs = "";
+    // FETCHING DATA FROM DATABASE
+    $resultAllBlog = mysqli_query($link, $queryAllBlog);
 } else {
-    if (isset($_GET["category"])) {
-        $slug = $_GET["category"];
-        $categories = mysqli_query($link, "SELECT * FROM categories WHERE slug = '$slug'");
-        $category = mysqli_fetch_assoc($categories);
-        $category_id = $category["id"];
-        $data = mysqli_query($link, "SELECT * FROM blogs WHERE category_id = $category_id");
-        $total_data = mysqli_num_rows($data);
-        $limit = 12;
-        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
-        $first_page = ($page > 1) ? ($page * $limit) - $limit : 0;
-        $total_page = "";
+    $data = mysqli_query($link, "SELECT * FROM blogs");
+    $total_data = mysqli_num_rows($data);
+    $limit = 12;
+    $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+    $first_page = ($page > 1) ? ($page * $limit) - $limit : 0;
+    $total_page = "";
 
-        $previous = $page - 1;
-        $next = $page + 1;
-        $total_page = ceil($total_data / $limit);
+    $previous = $page - 1;
+    $next = $page + 1;
+    $total_page = ceil($total_data / $limit);
 
-        $queryAllBlog = "SELECT * FROM blogs WHERE category_id = $category_id ORDER BY published_at DESC limit $first_page, $limit";
-        $blogs = "";
-        // FETCHING DATA FROM DATABASE
-        $resultAllBlog = mysqli_query($link, $queryAllBlog);
-    } else {
-        $data = mysqli_query($link, "SELECT * FROM blogs");
-        $total_data = mysqli_num_rows($data);
-        $limit = 12;
-        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
-        $first_page = ($page > 1) ? ($page * $limit) - $limit : 0;
-        $total_page = "";
-
-        $previous = $page - 1;
-        $next = $page + 1;
-        $total_page = ceil($total_data / $limit);
-
-        $queryAllBlog = "SELECT * FROM blogs ORDER BY published_at DESC limit $first_page, $limit";
-        $blogs = "";
-        // FETCHING DATA FROM DATABASE
-        $resultAllBlog = mysqli_query($link, $queryAllBlog);
-    }
+    $queryAllBlog = "SELECT * FROM blogs ORDER BY published_at DESC limit $first_page, $limit";
+    $blogs = "";
+    // FETCHING DATA FROM DATABASE
+    $resultAllBlog = mysqli_query($link, $queryAllBlog);
 }
