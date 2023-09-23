e31ee8854ad5a2222173284b097886dacec92bb1
hanif415 <hanifamrullah42@gmail.com>
2023-09-23 18:31:08 +0700

    fix

--- layouts/blogs/blogs.php
+++ layouts/blogs/blogs.php
@@ -219,8 +219,12 @@ include('../../backend/getBanner.php');
                 <div class="col-lg-2 col-6">
                     <p>MY ADS</p>
                     <ul>
-                        <li>About Us</li>
-                        <li>Check Our Blog</li>
+                        <a href="#about" style="color: white;">
+                            <li>About Us</li>
+                        </a>
+                        <a href="layouts/blogs/blogs.php" target="_blank" style="color: white;">
+                            <li>Check Our Blog</li>
+                        </a>
                     </ul>
                 </div>
                 <div class="col-lg-3 col-6">
@@ -241,14 +245,14 @@ include('../../backend/getBanner.php');
                                 <div class="col-8">
                                     <p>Contact Us</p>
                                     <ul>
-                                        <li><a href="#"><i class="fa fa-phone"></i> 010-020-0340</a></li>
-                                        <li><a href="#"><i class="fa fa-envelope"></i> digimedia@company.com</a></li>
+                                        <li><a href="https://wa.me/6282126271748" target="_blank"><i class="fa fa-whatsapp"></i> 0821-2627-1748</a></li>
+                                        <li><a href="#flyontech.group@gmail.com"><i class="fa fa-envelope"></i> flyontech.group@gmail.com</a></li>
                                     </ul>
                                 </div>
                                 <div class="col-4">
                                     <p>Follow Us</p>
                                     <ul class="social-media">
-                                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                        <li><a href="https://instagram.com/myads.id?igshid=NzZhOTFlYzFmZQ=="><i class="fa fa-instagram"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
